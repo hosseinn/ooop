18148c6ebce9f4332e2ca4d7450f27d8041986ef
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-03-08 08:14:15 +0000

    OOOP and GSI additions for 2.1

---
+++ trunk/addsdf.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+localize -m -l fr -f fr-gallery.sdf
+localize -m -l fr -f fr-patches.sdf
+localize -m -l fr -f fr-premium.sdf
+localize -m -l hu -f hu-gallery.sdf
+localize -m -l hu -f hu-patches.sdf
+localize -m -l hu -f hu-premium.sdf
+localize -m -l it -f it-gallery.sdf
+localize -m -l it -f it-patches.sdf
+localize -m -l it -f it-premium.sdf
+localize -m -l ka -f ka-gallery.sdf
+localize -m -l ka -f ka-patches.sdf
+localize -m -l ka -f ka-premium.sdf
+localize -m -l tr -f tr-gallery.sdf
+localize -m -l tr -f tr-patches.sdf
+localize -m -l tr -f tr-premium.sdf
