1cbc8a1ff719b5b94c6e7b03cd53a32082f8a365
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-04-05 14:52:31 +0000

    Updated language update file

--- trunk/addsdf.sh
+++ trunk/addsdf.sh
@@ -3,12 +3,14 @@
 localize -m -l fr -f fr-gallery.sdf
 localize -m -l fr -f fr-patches.sdf
 localize -m -l fr -f fr-premium.sdf
+localize -m -l hu -f hu_az.sdf
 localize -m -l hu -f hu-gallery.sdf
 localize -m -l hu -f hu-patches.sdf
 localize -m -l hu -f hu-premium.sdf
 localize -m -l it -f it-gallery.sdf
 localize -m -l it -f it-patches.sdf
 localize -m -l it -f it-premium.sdf
+localize -m -l ka -f GSI_ka.sdf
 localize -m -l ka -f ka-gallery.sdf
 localize -m -l ka -f ka-patches.sdf
 localize -m -l ka -f ka-premium.sdf
