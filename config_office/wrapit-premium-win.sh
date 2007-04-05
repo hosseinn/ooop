e56395179f5f4e8774dd70144bdabe6e5377cdbb
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-04-05 08:30:52 +0000

    Using prebuilt mozilla binaries for Windows

--- config_office/wrapit-premium-win.sh
+++ config_office/wrapit-premium-win.sh
@@ -12,7 +12,7 @@ setenv myANTHOME "/cygdrive/c/apache-ant-1.6.5"
 setenv myCLHOME "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Vc7"
 setenv myNMAKE_PATH "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Vc7/bin"
 setenv myCSCPATH "/cygdrive/c/WINDOWS/Microsoft.NET/Framework/v1.1.4322"
-setenv myMIDLPATH "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/Tools/Bin" 
+setenv myMIDLPATH "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/Tools/Bin"
 setenv myNSISPATH "/cygdrive/c/Program Files/NSIS"
 
 setenv PATH "${PATH}:${myIDE_PATH}"
@@ -22,7 +22,7 @@ setenv my_LANGUAGES "hu de fr it tr ka"
 
 setenv BUILDNUMBER "OxygenOffice Professional 2.2 - OxygenOffice Build 1"
 
-./configure --with-lang="$my_LANGUAGES" --with-mspdb-path="$myMSPDB_PATH" --with-frame-home="$myFWSDK_PATH" --with-jdk-home="$myJSDK_PATH" --with-use-shell=tcsh --with-psdk-home="$myPSDK_PATH" --with-directx-home="$myDX_PATH" --with-ant-home=$myANTHOME --with-cl-home="$myCLHOME" --with-nmake-path="$myNMAKE_PATH" --with-csc-path="$myCSCPATH" --with-nsis-path="$myNSISPATH"  --with-build-version="$BUILDNUMBER"
+./configure --with-lang="$my_LANGUAGES" --with-mspdb-path="$myMSPDB_PATH" --with-frame-home="$myFWSDK_PATH" --with-jdk-home="$myJSDK_PATH" --with-use-shell=tcsh --with-psdk-home="$myPSDK_PATH" --with-directx-home="$myDX_PATH" --with-ant-home=$myANTHOME --with-cl-home="$myCLHOME" --with-nmake-path="$myNMAKE_PATH" --with-csc-path="$myCSCPATH" --with-nsis-path="$myNSISPATH"  --with-build-version="$BUILDNUMBER" --disable-build-mozilla
 
 # supply temp dir
 echo setenv TEMP /tmp >>../winenv.set
