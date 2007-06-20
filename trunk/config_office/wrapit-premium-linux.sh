11a469471829652cc1b2818ce9e35662159ddeb8
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-06-20 11:55:41 +0000

    Wrapit scripts for 2.2.1

--- trunk/config_office/wrapit-premium-linux.sh
+++ trunk/config_office/wrapit-premium-linux.sh
@@ -14,10 +14,10 @@ ANTHOME="/usr/local/apache-ant-1.6.5"
 JAVAHOME="/usr/local/jdk1.5.0-10"
 
 # Build number
-BUILDNUMBER="OxygenOffice Professional 2.2 - OxygenOffice Build 1"
+BUILDNUMBER="OxygenOffice Professional 2.2.1 - OxygenOffice Build 1"
 
 # desired languages
-MYLANGUAGES="hu de fr it tr ka"
+MYLANGUAGES="hu de fr it tr ka fi pl"
 
 # Configure flags
 CONFIGUREFLAGS="--enable-kde --enable-gtk --enable-build-mozilla --with-system-nas  --enable-vba"
--- trunk/config_office/wrapit-premium-win.sh
+++ trunk/config_office/wrapit-premium-win.sh
@@ -18,9 +18,9 @@ setenv myNSISPATH "/cygdrive/c/Program Files/NSIS"
 setenv PATH "${PATH}:${myIDE_PATH}"
 
 # desired languages
-setenv my_LANGUAGES "hu de fr it tr ka"
+setenv my_LANGUAGES "hu de fr it tr ka fi pl"
 
-setenv BUILDNUMBER "OxygenOffice Professional 2.2 - OxygenOffice Build 1"
+setenv BUILDNUMBER "OxygenOffice Professional 2.2.1 - OxygenOffice Build 1"
 
 ./configure --with-lang="$my_LANGUAGES" --with-mspdb-path="$myMSPDB_PATH" --with-frame-home="$myFWSDK_PATH" --with-jdk-home="$myJSDK_PATH" --with-use-shell=tcsh --with-psdk-home="$myPSDK_PATH" --with-directx-home="$myDX_PATH" --with-ant-home=$myANTHOME --with-cl-home="$myCLHOME" --with-nmake-path="$myNMAKE_PATH" --with-csc-path="$myCSCPATH" --with-nsis-path="$myNSISPATH"  --with-build-version="$BUILDNUMBER" --disable-build-mozilla --enable-vba
 
