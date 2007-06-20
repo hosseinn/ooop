21d9fef43522da620094040bcc4c8c564a602a63
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-06-20 12:00:33 +0000

    Added translations, deleting integrated translation.

--- trunk/zzz_ka/addsdf-ka.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-localize -m -l ka -f ka.sdf
-localize -m -l ka -f ka-gallery.sdf
-localize -m -l ka -f ka-patches.sdf
-localize -m -l ka -f ka-premium.sdf
-localize -m -l ka -f ka-ooopteam.sdf
-localize -m -l en-US -f ooopteam.sdf
