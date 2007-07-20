63ded763655cb2def16507ee1193c51f6dd2d27b
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-07-20 16:25:51 +0000

    Updated tool for extension creating

--- trunk/utils/documents/create_templates.sh
+++ trunk/utils/documents/create_templates.sh
@@ -3,6 +3,9 @@ version=2.3.0.1
 echo [+] START...
 mkdir -p ../output
 
+for mode in separated unified
+do
+
 for folder in templates templates-nonfree
 do
 
@@ -22,8 +25,14 @@ do
     do
 
       echo [+] Processing $templates folder, $languages language...
+      if [ $mode="unified" ];
+      then
       mkdir -p ./_TEMP_/template/$templates
       cp -f ../../extras/source/premium/$folder/$templates/lang/$languages/*.* ./_TEMP_/template/$templates/
+      else
+      mkdir -p ./_TEMP_/template/$languages/$templates/
+      cp -f ../../extras/source/premium/$folder/$templates/lang/$languages/*.* ./_TEMP_/template/$languages/$templates/
+      fi
       #  /EXCLUDE:cretwork.exc
 
     done
@@ -39,21 +48,26 @@ do
     cp -f ../../documents/license/extension/readme_*.txt ./_TEMP_/readmes/
 
     echo [+] Adding description information...
-    echo "   <identifier value=\""net.sf.ooop.oxygenoffice.gallery.free.$languages"\" />" > description.xml.middle
+    echo "   <identifier value=\""net.sf.ooop.oxygenoffice.gallery.free.$languages.$mode"\" />" > description.xml.middle
     echo "   <version value=\""$version"\" />" >> description.xml.middle
 
     cat description.xml.begin > ./_TEMP_/description.xml & cat description.xml.middle >> ./_TEMP_/description.xml & cat description.xml.end >> ./_TEMP_/description.xml
     rm -f description.xml.middle
 
-    echo [+] Remove OOOP-$folder-$version.oxt...
-    rm -f ../output/OOOP-$folder-$languages-$version.oxt
+    echo [+] Remove OOOP-$folder-$mode-$languages-$version.oxt
+    rm -f ../output/OOOP-$folder-$mode-$languages-$version.oxt
 
-    echo [+] Creating empty OOOP-$folder-$languages-$version.oxt...
-    cp -f TemplatePackage.oxt ../output/OOOP-$folder-$languages-$version.oxt
+    echo [+] Creating empty OOOP-$folder-$mode-$languages-$version.oxt...
+    if [ $mode="unified" ];
+    then
+    cp -f TemplatePackage.oxt ../output/OOOP-$folder-$mode-$languages-$version.oxt
+    else
+    cp -f MultiLanguageTemplatePackage.oxt ../output/OOOP-$folder-$mode-$languages-$version.oxt
+    fi
 
-    echo [+] Compressing to OOOP-$folder-$languages-$version.oxt...
+    echo [+] Compressing to OOOP-$folder-$mode-$languages-$version.oxt...
     cd ./_TEMP_
-    zip -r -9 ../../output/OOOP-$folder-$languages-$version.oxt *
+    zip -r -9 ../../output/OOOP-$folder-$mode-$languages-$version.oxt *
     cd ..
 
     echo [+] Remove TEMP...
@@ -72,26 +86,32 @@ do
   cp -f ../../documents/license/extension/readme_*.txt ./_TEMP_ML_/readmes/
 
   echo [+] Adding description information...
-  echo "   <identifier value=\""net.sf.ooop.oxygenoffice.gallery.free.multi_language"\" />" > description.xml.middle
+  echo "   <identifier value=\""net.sf.ooop.oxygenoffice.gallery.free.multi_language.$mode"\" />" > description.xml.middle
   echo "   <version value=\""$version"\" />" >> description.xml.middle
 
   cat description.xml.begin > ./_TEMP_ML_/description.xml & cat description.xml.middle >> ./_TEMP_ML_/description.xml & cat description.xml.end >> ./_TEMP_ML_/description.xml
   rm -f description.xml.middle
 
-  echo [+] Remove OOOP-$folder-all-$version.oxt...
-  rm -f ../output/OOOP-$folder-all-$version.oxt
+  echo [+] Remove OOOP-$folder-$mode-all-$version.oxt...
+  rm -f ../output/OOOP-$folder-$mode-all-$version.oxt
 
-  echo [+] Creating empty OOOP-$folder-all-$version.oxt...
-  cp -f TemplatePackage.oxt ../output/OOOP-$folder-all-$version.oxt
+  echo [+] Creating empty OOOP-$folder-$mode-all-$version.oxt...
+    if [ $mode="unified" ];
+    then
+    cp -f TemplatePackage.oxt ../output/OOOP-$folder-$mode-all-$version.oxt
+    else
+    cp -f MultiLanguageTemplatePackage.oxt ../output/OOOP-$folder-$mode-all-$version.oxt
+    fi
 
   cd ./_TEMP_ML_
-  zip -r -9 ../../output/OOOP-$folder-all-$version.oxt *
+  zip -r -9 ../../output/OOOP-$folder-$mode-all-$version.oxt *
   cd ..
 
   rm -fr ./_TEMP_ML_
 
 done
 
+done
 
 echo [+] Finished, exiting and go home...
 
