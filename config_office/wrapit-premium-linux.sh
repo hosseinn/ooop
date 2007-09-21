34eb9d7eb5e430f3b57bcd0c21cf92dca7bdfaf0
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-09-21 09:14:12 +0000

    Updated warpit scripts

--- config_office/wrapit-premium-linux.sh
+++ config_office/wrapit-premium-linux.sh
@@ -14,12 +14,15 @@ ANTHOME="/usr/local/apache-ant-1.6.5"
 JAVAHOME="/usr/local/jdk1.5.0-10"
 
 # Build number
-BUILDNUMBER="OxygenOffice Professional 2.2.1 - OxygenOffice Build 1"
+BUILDNUMBER="OxygenOffice Professional 2.3.0 - OxygenOffice Build 1"
+
+# Builder name
+BUILDER="OxygenOffice Professional Team"
 
 # desired languages
 MYLANGUAGES="hu de fr it tr ka fi pl"
 
 # Configure flags
-CONFIGUREFLAGS="--enable-kde --enable-gtk --enable-build-mozilla --with-system-nas  --enable-vba"
+CONFIGUREFLAGS="--enable-kde --enable-gtk --enable-build-mozilla --with-system-nas  --enable-vba --enable-evolution2 --enable-dbus --enable-lockdown"
 
-./configure --with-lang="${MYLANGUAGES}" --with-jdk-home=${JAVAHOME} --with-ant-home=${ANTHOME} --with-package-format="${PKGFMTS}" ${CONFIGUREFLAGS} --with-build-version="${BUILDNUMBER}"
+./configure --with-lang="${MYLANGUAGES}" --with-jdk-home=${JAVAHOME} --with-ant-home=${ANTHOME} --with-package-format="${PKGFMTS}" ${CONFIGUREFLAGS} --with-build-version="${BUILDNUMBER}" --with-vendor="${BUILDER}"
