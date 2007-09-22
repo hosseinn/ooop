4c048afa410e78000f85c72f10a8e6c660f02093
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-09-22 11:15:20 +0000

    Updated lingucomponent

---
+++ trunk/lingucomponent/source/spellcheck/hunspell/makefile.mk
@@ -0,0 +1,88 @@
+#*************************************************************************
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.3.52.6 $
+#
+#   last change: $Author: nemeth $ $Date: 2007/09/20 10:17:43 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#   GNU Lesser General Public License Version 2.1
+#   =============================================
+#   Copyright 2005 by Sun Microsystems, Inc.
+#   901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License version 2.1, as published by the Free Software Foundation.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with this library; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#   MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ = ..$/..$/..
+
+PRJNAME	= lingucomponent
+TARGET	= hunspell
+USE_DEFFILE = TRUE
+
+
+
+#----- Settings ---------------------------------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Files --------------------------------------------------------
+
+.IF "$(SYSTEM_HUNSPELL)" == "YES"
+@all:
+	@echo "Using system hunspell..."
+.ENDIF
+
+# all_target: ALLTAR DICTIONARY
+all_target: ALLTAR
+
+.IF "$(GUI)" == "UNX"
+CDEFS+=-DOPENOFFICEORG
+.ENDIF
+
+SLOFILES=	\
+		$(SLO)$/affentry.obj \
+		$(SLO)$/affixmgr.obj \
+		$(SLO)$/csutil.obj \
+		$(SLO)$/phonet.obj \
+		$(SLO)$/hashmgr.obj \
+		$(SLO)$/suggestmgr.obj \
+		$(SLO)$/hunspell.obj
+
+SHL1TARGET= 	$(TARGET)
+
+.IF "$(GUI)" == "UNX"
+SHL1STDLIBS= 	$(ICUUCLIB)
+.ENDIF
+
+# build DLL
+SHL1DEPN=
+SHL1IMPLIB=	i$(TARGET)
+SHL1LIBS=	$(SLB)$/$(TARGET).lib
+SHL1DEF=	$(MISC)$/$(SHL1TARGET).def
+DEF1NAME=	$(SHL1TARGET)
+.IF "$(GUI)$(COM)"=="WNTGCC"
+DEFLIB1NAME =$(TARGET)
+.ENDIF
+
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : target.mk
+
