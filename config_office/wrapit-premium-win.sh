7b182ce6710c11d8b2352e1520e8200fc9257b04
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-10-12 11:43:07 +0000

    TCSH -> BASH on Windows

--- config_office/wrapit-premium-win.sh
+++ config_office/wrapit-premium-win.sh
@@ -1,30 +1,31 @@
+#!/bin/bash
 # root of build tree
-setenv my_BUILD_ROOT /cygdrive/c/ooo2
+my_BUILD_ROOT="/cygdrive/c/ooo2"
 
 
-setenv myIDE_PATH "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/IDE"
-setenv myMSPDB_PATH "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/IDE"
-setenv myFWSDK_PATH "/cygdrive/c/Program Files/Microsoft.NET/SDK/v1.1"
-setenv myJSDK_PATH "/cygdrive/c/j2sdk1.4.2_10"
-setenv myPSDK_PATH "/cygdrive/c/Program Files/Microsoft Platform SDK"
-setenv myDX_PATH "/cygdrive/c/DXSDK"
-setenv myANTHOME "/cygdrive/c/apache-ant-1.6.5"
-setenv myCLHOME "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Vc7"
-setenv myNMAKE_PATH "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Vc7/bin"
-setenv myCSCPATH "/cygdrive/c/WINDOWS/Microsoft.NET/Framework/v1.1.4322"
-setenv myMIDLPATH "/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/Tools/Bin"
-setenv myNSISPATH "/cygdrive/c/Program Files/NSIS"
+myIDE_PATH="/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/IDE"
+myMSPDB_PATH="/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/IDE"
+myFWSDK_PATH="/cygdrive/c/Program Files/Microsoft.NET/SDK/v1.1"
+myJSDK_PATH="/cygdrive/c/j2sdk1.4.2_10"
+myPSDK_PATH="/cygdrive/c/Program Files/Microsoft Platform SDK"
+myDX_PATH="/cygdrive/c/DXSDK"
+myANTHOME="/cygdrive/c/apache-ant-1.6.5"
+myCLHOME="/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Vc7"
+myNMAKE_PATH="/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Vc7/bin"
+myCSCPATH="/cygdrive/c/WINDOWS/Microsoft.NET/Framework/v1.1.4322"
+myMIDLPATH="/cygdrive/c/Program Files/Microsoft Visual Studio .NET 2003/Common7/Tools/Bin"
+myNSISPATH="/cygdrive/c/Program Files/NSIS"
 
-setenv PATH "${PATH}:${myIDE_PATH}"
+PATH "${PATH}:${myIDE_PATH}"
 
 # desired languages
-setenv my_LANGUAGES "hu de fr it tr ka fi pl nl pt-BR es"
+my_LANGUAGES="hu de fr it tr ka fi pl nl pt-BR es"
 
-setenv BUILDNUMBER "OxygenOffice Professional 2.3.0 - OxygenOffice Build 2"
+BUILDNUMBER="OxygenOffice Professional 2.3.0 - OxygenOffice Build 2"
 
-setenv BUILDER "OxygenOffice Professional Team"
+BUILDER="OxygenOffice Professional Team"
 
-./configure --with-lang="$my_LANGUAGES" --with-mspdb-path="$myMSPDB_PATH" --with-frame-home="$myFWSDK_PATH" --with-jdk-home="$myJSDK_PATH" --with-use-shell=tcsh --with-psdk-home="$myPSDK_PATH" --with-directx-home="$myDX_PATH" --with-ant-home=$myANTHOME --with-cl-home="$myCLHOME" --with-nmake-path="$myNMAKE_PATH" --with-csc-path="$myCSCPATH" --with-nsis-path="$myNSISPATH"  --with-build-version="$BUILDNUMBER" --with-vendor="$BUILDER" --disable-build-mozilla --enable-vba
+./configure --with-lang="$my_LANGUAGES" --with-mspdb-path="$myMSPDB_PATH" --with-frame-home="$myFWSDK_PATH" --with-jdk-home="$myJSDK_PATH" --with-use-shell=bash --with-psdk-home="$myPSDK_PATH" --with-directx-home="$myDX_PATH" --with-ant-home=$myANTHOME --with-cl-home="$myCLHOME" --with-nmake-path="$myNMAKE_PATH" --with-csc-path="$myCSCPATH" --with-nsis-path="$myNSISPATH"  --with-build-version="$BUILDNUMBER" --with-vendor="$BUILDER" --disable-build-mozilla --enable-vba
 
 # supply temp dir
 echo setenv TEMP /tmp >>../winenv.set
