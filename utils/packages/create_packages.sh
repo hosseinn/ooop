8169329cc6ada1a8899e541f6cb48945ca4695c7
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2007-12-27 14:14:06 +0000

    New utility to created packages for ooo-build system

---
+++ utils/packages/create_packages.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+version=2.3.1.2
+echo [+] START...
+mkdir -p ../output
+
+rm -fr ./_TEMP_
+for folder in templates templates-nonfree
+do
+      
+      mkdir -p ./_TEMP_/templates/premium/$folder
+      rsync -avv --progress --human-readable --exclude-from=exclude.exc ../../extras/source/premium/$folder/* ./_TEMP_/templates/premium/$folder/
+done
+
+for folder in samples samples-nonfree
+do
+      
+      mkdir -p ./_TEMP_/samples/premium/$folder
+      rsync -avv --progress --human-readable --exclude-from=exclude.exc ../../extras/source/premium/$folder/* ./_TEMP_/samples/premium/$folder/
+done
+
+for folder in gallery
+do
+      
+      mkdir -p ./_TEMP_/gallery/premium/$folder
+      rsync -avv --progress --human-readable --exclude-from=exclude.exc ../../extras/source/$folder/* ./_TEMP_/gallery/$folder/
+done
+
+for folder in truetype
+do
+      
+      mkdir -p ./_TEMP_/fonts/premium/$folder
+      rsync -avv --progress --human-readable --exclude-from=exclude.exc ../../extras/source/$folder/* ./_TEMP_/fonts/$folder/
+done
+
+for package in templates samples gallery fonts
+do
+    echo [+] Compressing to OOOP-$package-pack-$version.zip...
+    cd ./_TEMP_/$package
+    zip -r -9 ../../../output/OOOP-$package-pack-$version.zip *
+    cd ../../
+
+done
+
+    echo [+] Remove TEMP...
+    rm -fr ./_TEMP_
+
+echo [+] Finished, exiting and go home...
+
+exit 0
