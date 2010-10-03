17b3c7a6c406ff8f37e4d7cae4d3c258b7a84f83
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2010-10-03 07:47:23 +0000

    Updated script to handle en_US as default language. You do not have to add en_US to supported_languages

--- trunk/utils/translation/update-po-files.sh
+++ trunk/utils/translation/update-po-files.sh
@@ -2,32 +2,45 @@
 
 # Update supported languages from properties file to po and pot files
 
+SCRIPTNAME="Update PO files from properties files"
+VERSION="V1.0.0"
+SCRIPTFILE=`basename $0`
+
+echo -e "\n$SCRIPTNAME - $VERSION\n"
+
+echo "[.] Starting conversion..."
 CURRENT_DIR=`pwd`
 
 PROPERTIES_DIR="$CURRENT_DIR/dialogs"
 PO_DIR="$CURRENT_DIR/po"
 TEMP_DIR="$CURRENT_DIR/temp"
 
+SUPPORTED_LANGUAGES="`cat $PO_DIR/supported_languages` en_US"
+
 MSGCAT_OPTION="--use-first"
 
 mkdir -p $PO_DIR/template $TEMP_DIR/template
 
-for language in `cat $PO_DIR/supported_languages`
+for language in $SUPPORTED_LANGUAGES
 do
+	echo "[+] Preparing $language language..."
 	cp $PROPERTIES_DIR/*_$language.properties $TEMP_DIR/
 	cp $PROPERTIES_DIR/*en_US.properties $TEMP_DIR/template
-	cd $TEMP_DIR/template
-	for i in *_en_US.properties
-	do
-		j=`echo $i | sed "s/en_US/$language/g"`
-		mv "$i" "$j"
-	done
+	if [ "$language" != "en_US" ]
+	then
+		cd $TEMP_DIR/template
+		for i in *_en_US.properties
+		do
+			j=`echo $i | sed "s/en_US/$language/g"`
+			mv "$i" "$j"
+		done
+	fi
 done
 
-moz2po --input $PROPERTIES_DIR --output $TEMP_DIR --template $TEMP_DIR/template --exclude .default
+moz2po --input $PROPERTIES_DIR --output $TEMP_DIR --template $TEMP_DIR/template --exclude "*.xdl" --exclude "*.default"
 
 # Checking for supported languages
-for language in `cat $PO_DIR/supported_languages`
+for language in $SUPPORTED_LANGUAGES
 do
 # Make language dependent directories and move a po files there
 	echo "[+] Now processing $language language..."
@@ -44,9 +57,9 @@ do
 			then
             			mv $po_temp "$PO_DIR/$language/$po_file"
             			chmod 644 "$PO_DIR/$language/$po_file"
-            			echo "[.] $po_file was succesfully updated"
+            			echo "[.] $po_file was succesfully updated."
         		else
-            			echo "[!] $po_file was not updated"
+            			echo "[!] $po_file was not updated."
         		fi
     		else
         		echo "[+] Adding $po_file..."
