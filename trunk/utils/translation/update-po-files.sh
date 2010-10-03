549787c57522a02f5637453f804465a7a52bfdc7
kami_ <kami_@24a239b5-211e-0410-a31f-aed1b7a89b4d>
2010-10-03 08:12:01 +0000

    Update update-po-files.sh script to make it configurable, displays help and configuration

--- trunk/utils/translation/update-po-files.sh
+++ trunk/utils/translation/update-po-files.sh
@@ -8,12 +8,51 @@ SCRIPTFILE=`basename $0`
 
 echo -e "\n$SCRIPTNAME - $VERSION\n"
 
-echo "[.] Starting conversion..."
-CURRENT_DIR=`pwd`
+# Help, I need somebody
+function USAGE ()
+{
+    echo ""
+    echo "USAGE: "
+    echo "    $SCRIPTFILE [SWITCH...]"
+    echo ""
+    echo "OPTIONS:"
+    echo "        -p, --properties-dir DIRECTORY - Location of properties dir, it will converted to po file"
+    echo "        -po, --po-dir DIRECTORY        - Location of po dir"
+    echo "        -t, --temp DIRECTORY           - Location of temporary folder"
+    echo "        -?, -h, --help                 - Displays this help"
+    echo ""
+    echo "EXAMPLE:"
+    echo "    $SCRIPTFILE"
+    echo ""
+    exit $E_OPTERROR
+}
 
+# Set default setting - customer profile
+
+CURRENT_DIR=`pwd`
 PROPERTIES_DIR="$CURRENT_DIR/dialogs"
 PO_DIR="$CURRENT_DIR/po"
 TEMP_DIR="$CURRENT_DIR/temp"
+
+# Process arguments
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -p|--properties-dir) PROPERTIES_DIR="$2"; shift;;
+        -po|--po-dir) PO_DIR="$2"; shift;;
+        -t|--temp-dir) TEMP_DIR="$2"; shift;;
+        -?|-h|--help) USAGE;;
+	*) echo -e "\nERROR: Unknown parameter\n"; USAGE; exit $E_OPTERROR;;
+    esac
+    shift
+done
+
+echo -e "Current settings:\n-------------------------------------------------------------"
+echo    "Properties directory      : $PROPERTIES_DIR"
+echo    "PO directory              : $PO_DIR"
+echo -e "TEMP directory            : $TEMP_DIR\n"
+
+echo "[.] Starting conversion..."
 
 SUPPORTED_LANGUAGES="`cat $PO_DIR/supported_languages` en_US"
 
